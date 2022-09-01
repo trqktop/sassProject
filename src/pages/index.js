12d3f84a0719ba34075dfd724ccde080737be58c
trqktop <trqktop@yandex.ru>
2022-09-01 13:10:37 +0300

    feat/html

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,1 @@
 import './index.css'; // добавьте импорт главного файла стилей для webpack
-
-const numbers = [2, 3, 5];
-
-// Стрелочная функция. Не запнётся ли на ней Internet Explorer?
-const doubledNumbers = numbers.map(number => number * 2);
-
-console.log(doubledNumbers); // 4, 6, 10
